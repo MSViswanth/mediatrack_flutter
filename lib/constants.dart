1265c954f788b2e79832319bf28789540a07e3b4
Viswanth <30171132+Viswanth1038@users.noreply.github.com>
2020-06-20 15:04:07 +0530

    Initial Commit

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kAPIKey = '2f32c2238114aa8b5cf4aca53923e6d6';
+const kAPIReadAccessToken =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjMyYzIyMzgxMTRhYThiNWNmNGFjYTUzOTIzZTZkNiIsInN1YiI6IjViOWUzNWY2YzNhMzY4NDQwMDAxODljNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7PHPj_CuBfm5-ES1DlkzLKH9xk3-4qhC3oy_Us_EhPI';
+
+const kOptionsStyle = TextStyle(
+  fontSize: 20,
+);
+String baseUrl = 'http://image.tmdb.org/t/p/';
+String posterSize = 'w500';
+String profileSize = 'w780';
+String url = 'https://flutter.dev/images/flutter-logo-sharing.png';
+String backdropSize = 'w1280';
