66ec31bfe9d77cd35a76c8a8c374cfcd9c6fec22
Viswanth <viswanth.mullapudi28@gmail.com>
2020-07-29 21:41:34 +0530

    removed appendtoresponse

--- lib/providers/tv_provider.dart
+++ lib/providers/tv_provider.dart
@@ -13,8 +13,9 @@ class TVProvider with ChangeNotifier {
 
   void updateDetails(List<Show> show, int index) async {
     try {
-      Map movieUpdated = await tmdb.v3.movies.getDetails(show[index].id,
-          appendToResponse: 'release_dates,similar_movies,recommendations');
+      Map movieUpdated = await tmdb.v3.movies.getDetails(
+        show[index].id,
+      );
       show[index] = Show.fromJson(movieUpdated);
       // print(movie[index].homepage);
 
