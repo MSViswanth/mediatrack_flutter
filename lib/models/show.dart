aefcc86fc4b492f7940aa8caa1bcd7ff5cd88b19
Prudhvi-19 <chundru.prudhvisairam@gmail.com>
2020-07-29 19:17:03 +0530

    Adding show.dart

---
+++ lib/models/show.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'movie.dart';
+part 'show.g.dart';
+
+@JsonSerializable(
+  fieldRename: FieldRename.snake,
+  nullable: true,
+  explicitToJson: true,
+)
+
+class Show{
+  String backdropPath;
+  List<CreatedBy> createdBy;
+  List<EpisodeRunTime> episodeRunTime;
+  String firstAirDate;
+  List<Genres> genres;
+  List<Languages> languages;
+
+}
