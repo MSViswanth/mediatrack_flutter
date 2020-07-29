d7c37d977f5142f0630238e8c8c1c76c148a21fd
Viswanth <viswanth.mullapudi28@gmail.com>
2020-07-29 21:52:04 +0530

    added horizontal list for tv shows

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mediatrack_flutter/components/horizontal_list.dart';
+import 'package:mediatrack_flutter/components/horizontal_list_tv.dart';
 
 import 'package:mediatrack_flutter/models/movie.dart';
 import 'package:mediatrack_flutter/models/show.dart';
@@ -16,6 +17,7 @@ class HomeScreen extends StatelessWidget {
     TVProvider tvProvider = Provider.of<TVProvider>(context);
     SettingsProvider settingsProvider = Provider.of<SettingsProvider>(context);
     bool isWaiting = moviesProvider.isWaiting;
+    bool isWaitingTv = tvProvider.isWaiting;
     List<Movie> popularMovies = moviesProvider.trendingMovies;
     List<Show> popularTVShows = tvProvider.trendingTVShows;
 
@@ -41,7 +43,7 @@ class HomeScreen extends StatelessWidget {
               child: Row(
                 children: <Widget>[
                   Text(
-                    'Trending',
+                    'Trending Movies',
                     style: GoogleFonts.lato(
                       textStyle: Theme.of(context).textTheme.headline6,
                     ),
@@ -59,6 +61,29 @@ class HomeScreen extends StatelessWidget {
                     ),
                   )
                 : HorizontalList(itemList: popularMovies),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Trending TV Shows',
+                    style: GoogleFonts.lato(
+                      textStyle: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(Icons.arrow_forward),
+                ],
+              ),
+            ),
+            isWaitingTv
+                ? Container(
+                    height: 200,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  )
+                : HorizontalListTV(itemList: popularTVShows),
             // HorizontalList(isWaiting: isWaiting, itemList: popularMovies),
             // HorizontalList(isWaiting: isWaiting, itemList: popularMovies),
             // HorizontalList(isWaiting: isWaiting, itemList: popularMovies),
