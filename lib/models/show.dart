483eeecadadcbbbd8bdc6866e8c45f022869b03d
Prudhvi-19 <chundru.prudhvisairam@gmail.com>
2020-07-29 20:45:39 +0530

    Add show.dart Testing My Push to Upstream

--- lib/models/show.dart
+++ lib/models/show.dart
@@ -7,14 +7,178 @@ part 'show.g.dart';
   nullable: true,
   explicitToJson: true,
 )
-
 class Show{
   String backdropPath;
   List<CreatedBy> createdBy;
-  List<EpisodeRunTime> episodeRunTime;
+  List<int> episodeRunTime;
   String firstAirDate;
   List<Genres> genres;
-  List<Languages> languages;
+  String homepage;
+  int id;
+  bool inProduction;
+  List<String> languages;
   String lastAirDate;
+  LastEpisodeToAir lastEpisodeToAir;
+  String name;
+  String nextEpisodeToAir;
+  List<Networks> networks;
+  int numberOfEpisodes;
+  int numberOfSeasons;
+  List<String> originCountry;
+  String originLanguage;
+  String originalName;
+  String overview;
+  int popularity;
+  String posterPath;
+  List<ProductionCompanies> productionCompanies;
+  List<Seasons> seasons;
+  String status;
+  String type;
+  int voteAverage;
+  int voteCount;
+
+  Show(
+      this.backdropPath,
+      this.createdBy,
+      this.episodeRunTime,
+      this.firstAirDate,
+      this.genres,
+      this.homepage,
+      this.id,
+      this.inProduction,
+      this.languages,
+      this.lastAirDate,
+      this.lastEpisodeToAir,
+      this.name,
+      this.nextEpisodeToAir,
+      this.networks,
+      this.numberOfEpisodes,
+      this.numberOfSeasons,
+      this.originalName,
+      this.originCountry,
+      this.originLanguage,
+      this.overview,
+      this.popularity,
+      this.posterPath,
+      this.productionCompanies,
+      this.seasons,
+      this.status,
+      this.type,
+      this.voteAverage,
+      this.voteCount,
+      );
+
+  factory Show.fromJson(Map<String, dynamic> json) => _$ShowFromJson(json);
+  Map<String, dynamic> toJson() => _$ShowToJson(this);
+
+}
+
+
+@JsonSerializable(
+  fieldRename: FieldRename.snake,
+  nullable: true,
+)
+class CreatedBy{
+  int id;
+  String creditId;
+  String name;
+  int gender;
+  String profilePath;
+
+  CreatedBy(
+      this.name,
+      this.id,
+      this.creditId,
+      this.gender,
+      this.profilePath,
+      );
+
+  factory CreatedBy.fromJson(Map<String, dynamic> json) => _$CreatedByFromJson(json);
+  Map<String, dynamic> toJson() => _$CreatedByToJson(this);
+}
+
+@JsonSerializable(
+  fieldRename: FieldRename.snake,
+  nullable: true,
+)
+class LastEpisodeToAir{
+  String airDate;
+  int episodeNumber;
+  int id;
+  String name;
+  String overview;
+  String productionCode;
+  int seasonNumber;
+  int showId;
+  String stillPath;
+  double voteAverage;
+  int voteCount;
+
+  LastEpisodeToAir(
+      this.id,
+      this.name,
+      this.voteCount,
+      this.voteAverage,
+      this.overview,
+      this.airDate,
+      this.episodeNumber,
+      this.productionCode,
+      this.seasonNumber,
+      this.showId,
+      this.stillPath,
+      );
+
+  factory LastEpisodeToAir.fromJson(Map<String, dynamic> json) => _$LastEpisodeToAirFromJson(json);
+  Map<String, dynamic> toJson() => _$LastEpisodeToAirToJson(this);
+
+
+}
+
+@JsonSerializable(
+  fieldRename: FieldRename.snake,
+  nullable: true,
+)
+class Networks{
+  String name;
+  int id;
+  String logoPath;
+  String originCountry;
+
+  Networks(
+      this.name,
+      this.id,
+      this.originCountry,
+      this.logoPath
+      );
+
+  factory Networks.fromJson(Map<String, dynamic> json) => _$NetworksFromJson(json);
+  Map<String, dynamic> toJson() => _$NetworksToJson(this);
+}
+
+@JsonSerializable(
+  fieldRename: FieldRename.snake,
+  nullable: true,
+)
+class Seasons{
+  String airDate;
+  String episodeCount;
+  int id;
+  String name;
+  String overview;
+  String posterPath;
+  int seasonNumber;
+
+  Seasons(
+      this.airDate,
+      this.episodeCount,
+      this.id,
+      this.name,
+      this.overview,
+      this.posterPath,
+      this.seasonNumber,
+      );
+
+  factory Seasons.fromJson(Map<String, dynamic> json) => _$SeasonsFromJson(json);
+  Map<String, dynamic> toJson() => _$SeasonsToJson(this);
 
 }
