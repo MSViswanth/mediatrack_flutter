31c7e5d09c6f4544053444557ef5810af22ed9a2
Prudhvi-19 <chundru.prudhvisairam@gmail.com>
2020-07-30 19:01:58 +0530

    More Data Added to Show Details in details_screen_tv.dart

--- lib/views/tvshow/details_screen_tv.dart
+++ lib/views/tvshow/details_screen_tv.dart
@@ -84,7 +84,7 @@ class _DetailsScreenTVShowState extends State<DetailsScreenTVShow> {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
                         Column(
-                          //mainAxisAlignment: MainAxisAlignment.spaceAround, //TODO
+                          mainAxisAlignment: MainAxisAlignment.end, //TODO
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             Container(
@@ -94,7 +94,7 @@ class _DetailsScreenTVShowState extends State<DetailsScreenTVShow> {
                                 widget.tvshow.originalName+' - '+widget.tvshow.originalLanguage,
                                 style: GoogleFonts.lato(
                                   textStyle: TextStyle(
-                                      fontSize: 21,
+                                      fontSize: 20,
                                       fontWeight: FontWeight.w600),
                                 ),
                               )
@@ -116,17 +116,59 @@ class _DetailsScreenTVShowState extends State<DetailsScreenTVShow> {
                               children: <Widget>[
                                 widget.tvshow.firstAirDate != ''
                                     ? Text(
-                                  '${DateTime.parse(widget.tvshow.firstAirDate).year}'+'  -  '+'${widget.tvshow.status=='Ended'? DateTime.parse(widget.tvshow.lastEpisodeToAir.airDate).year: 'Tomorrow'}',
+                                  '${DateTime.parse(widget.tvshow.firstAirDate).year}'+'  -  '+'${widget.tvshow.status=='Ended'? DateTime.parse(widget.tvshow.lastEpisodeToAir.airDate).year: 'Present'}',
                                   style: TextStyle(fontSize: 18),
                                 )
                                     : Text('Not Available'),
                                 SizedBox(
                                   width: 20,
                                 ),
+                                widget.tvshow.voteAverage != 0
+                                    ? Row(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.star,
+                                      color: Color(0xff90cea1),
+                                      size: 30,
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      widget.tvshow.voteAverage.toString(),
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                                    : Text(
+                                  'NR',
+                                  style: TextStyle(color: Colors.grey),
+                                ),
                               ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              width: 0.6 * size.width,
+                              child: widget.tvshow.seasons != null
+                                  ? Text(
+                                '${widget.tvshow.numberOfEpisodes} Episodes',
+                                style: GoogleFonts.lato(
+                                  textStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              )
+                                  : null,
                             ),
                           ],
                         ),
+
                         Container(
                           height: 200,
                           decoration: BoxDecoration(
@@ -195,9 +237,324 @@ class _DetailsScreenTVShowState extends State<DetailsScreenTVShow> {
                           width: 50,
                           child: LinearProgressIndicator(),
                         ),
+                        widget.tvshow.episodeRunTime == null
+                            ? Container(
+                          width: 50,
+                          child: LinearProgressIndicator(),
+                        )
+                            : widget.tvshow.episodeRunTime[0] != 0
+                            ? Text(
+                          widget.tvshow.episodeRunTime[0] ~/ 60 != 0 &&
+                              widget.tvshow.episodeRunTime[0] % 60 != 0
+                              ? '${widget.tvshow.episodeRunTime[0] ~/ 60}hr ${widget.tvshow.episodeRunTime[0] % 60}min'
+                              : widget.tvshow.episodeRunTime[0] ~/ 60 == 0
+                              ? '${widget.tvshow.episodeRunTime[0] % 60}min'
+                              : '${widget.tvshow.episodeRunTime[0] ~/ 60}hr',
+                          style: TextStyle(fontSize: 18),
+                        )
+                            : Text(''),
+                        Container(
+                          padding: EdgeInsets.all(3),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyText2
+                                    .color,
+                              ),
+                              borderRadius: BorderRadius.circular(3)),
+//                          child: Text(Provider.of<MovieProvider>(context)
+//                              .certification ==
+//                              '' ||
+//                              Provider.of<MovieProvider>(context)
+//                                  .certification ==
+//                                  null
+//                              ? 'NR'
+//                              : Provider.of<MovieProvider>(context)
+//                              .certification),
+                        child: Text('TV-MA'),
+                        ),
+                        GestureDetector(
+                          onTap : () {},
+                          child: Image.asset(
+                            'assets/images/imdb.png',
+                            scale: 1.5,
+                          ),
+                        ),
                       ],
                     ),
+                  ),
+                  widget.tvshow.homepage != null &&
+                      widget.tvshow.homepage != ''
+                      ? Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.all(16),
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Theme.of(context).accentColor,
+                    ),
+                    child: InkWell(
+                      onTap: () => launch(widget.tvshow.homepage),
+                      child: Center(
+                        child: Text(
+                          '\'' +
+                              widget.tvshow.name +
+                              '\'' +
+                              ' Website',
+                          style: TextStyle(
+                            color: Theme.of(context)
+                                .scaffoldBackgroundColor,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
                   )
+                      : Container(),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Overview',
+                      style: GoogleFonts.lato(
+                        textStyle: Theme.of(context).textTheme.headline6,
+                        color: Theme.of(context).accentColor,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                    EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+                    child: Text(
+                      widget.tvshow.overview,
+                      style: GoogleFonts.lato(
+                        textStyle: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Genres',
+                          style: GoogleFonts.lato(
+                            textStyle:
+                            Theme.of(context).textTheme.headline6,
+                            color: Theme.of(context).accentColor,
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Theme.of(context).accentColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(2),
+                    margin: EdgeInsets.all(14),
+                    height: 45,
+                    child: widget.tvshow.genres != null
+                        ? widget.tvshow.genres.length == 0
+                        ? Center(
+                      child: Text(
+                        'Not Available',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    )
+                        : ListView.builder(
+                        physics: BouncingScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: widget.tvshow.genres.length,
+                        itemBuilder: (context, genre) {
+                          return Container(
+                            // height: 25,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 5),
+                            alignment: Alignment.center,
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyText2
+                                    .color,
+                              ),
+                              borderRadius:
+                              BorderRadius.circular(20),
+                            ),
+                            child: Text(
+                              '${widget.tvshow.genres[genre].name}',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          );
+                        })
+                        : Text('Waiting...'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Spoken Languages',
+                          style: GoogleFonts.lato(
+                            textStyle:
+                            Theme.of(context).textTheme.headline6,
+                            color: Theme.of(context).accentColor,
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Theme.of(context).accentColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 25,
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: widget.tvshow.languages == null
+                        ? Center(child: Text('Waiting...'))
+                        : ListView.builder(
+                      physics: BouncingScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Container(
+                            margin: EdgeInsets.only(right: 16),
+                            child: Text(widget
+                                .tvshow.languages[index]));
+                      },
+                      itemCount: widget.tvshow.languages.length,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Production Countries',
+                          style: GoogleFonts.lato(
+                            textStyle:
+                            Theme.of(context).textTheme.headline6,
+                            color: Theme.of(context).accentColor,
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Theme.of(context).accentColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 25,
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: widget.tvshow.originCountry == null
+                        ? Center(child: Text('Waiting'))
+                        : widget.tvshow.originCountry.length != 0
+                        ? ListView.builder(
+                      physics: BouncingScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Container(
+                            margin: EdgeInsets.only(right: 16),
+                            child: Text(widget
+                                .tvshow
+                                .originCountry[index]));
+                      },
+                      itemCount:
+                      widget.tvshow.originCountry.length,
+                    )
+                        : Center(
+                      child: Text('Not Available'),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Production Companies',
+                          style: GoogleFonts.lato(
+                            textStyle:
+                            Theme.of(context).textTheme.headline6,
+                            color: Theme.of(context).accentColor,
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Theme.of(context).accentColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 30,
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: widget.tvshow.productionCompanies != null
+                        ? widget.tvshow.productionCompanies.length != 0
+                        ? ListView.builder(
+                        physics: BouncingScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: EdgeInsets.only(right: 16),
+                            child: widget
+                                .tvshow
+                                .productionCompanies[index]
+                                .logoPath ==
+                                null
+                                ? Center(
+                              child: Text(widget
+                                  .tvshow
+                                  .productionCompanies[
+                              index]
+                                  .name +
+                                  ' ' +
+                                  widget
+                                      .tvshow
+                                      .productionCompanies[
+                                  index]
+                                      .originCountry),
+                            )
+                                : Tooltip(
+                              message: widget
+                                  .tvshow
+                                  .productionCompanies[
+                              index]
+                                  .name +
+                                  ' ' +
+                                  widget
+                                      .tvshow
+                                      .productionCompanies[
+                                  index]
+                                      .originCountry,
+                              child: Image.network(baseUrl +
+                                  logoSize +
+                                  widget
+                                      .tvshow
+                                      .productionCompanies[
+                                  index]
+                                      .logoPath),
+                            ),
+                          );
+                        },
+                        itemCount:
+                        widget.tvshow.productionCompanies.length)
+                        : Center(
+                      child: Text('Not Available'),
+                    )
+                        : Container(
+                      child: Text('Waiting'),
+                    ),
+                  ),
                 ]
               ),
             )
