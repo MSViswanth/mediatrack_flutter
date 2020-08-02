d0635b4ceb05efce6fa471737338d739fe176fe5
Viswanth <viswanth.mullapudi28@gmail.com>
2020-08-02 15:34:27 +0530

    Added season names to horizontallistseason

--- lib/components/tvshow/season/horizontal_list_season.dart
+++ lib/components/tvshow/season/horizontal_list_season.dart
@@ -2,7 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mediatrack_flutter/constants.dart';
-import 'package:mediatrack_flutter/models/tvshow/tvshow.dart';
+import 'package:mediatrack_flutter/models/tvshow/season/season.dart';
+
 import 'package:mediatrack_flutter/providers/tvshow/season/season_provider.dart';
 import 'package:mediatrack_flutter/views/tvshow/season/details_screen_season.dart';
 import 'package:provider/provider.dart';
@@ -16,7 +17,7 @@ class HorizontalListSeasons extends StatelessWidget {
     @required this.tvshowName,
   });
 
-  final List<Seasons> itemList;
+  final List<Season> itemList;
   final String title = 'Seasons';
   final int tvId;
   final String backdropPath;
@@ -47,23 +48,24 @@ class HorizontalListSeasons extends StatelessWidget {
         ),
         Container(
           padding: EdgeInsets.all(5),
-          height: 200,
+          height: 240,
           child: ListView.builder(
             physics: BouncingScrollPhysics(),
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
               return Container(
+                width: 120,
                 margin: EdgeInsets.all(8),
-                child: GestureDetector(
+                child: InkWell(
                   onTap: () {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => DetailsScreenSeason(
-                            tvId: tvId,
-                            seasonNumber: itemList[index].seasonNumber,
+                            season: itemList[index],
                             backdropPath: backdropPath,
                             tvshowName: tvshowName,
+                            tvId: tvId,
                           ),
                         ));
                   },
@@ -77,75 +79,90 @@ class HorizontalListSeasons extends StatelessWidget {
                   //     ),
                   //   );
                   // },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          offset: Offset(5, 5),
-                          blurRadius: 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              offset: Offset(5, 5),
+                              blurRadius: 3,
+                            ),
+                          ],
+                          borderRadius: BorderRadius.circular(12),
                         ),
-                      ],
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: ClipRRect(
-                      //Removed Aspect Ratio. Add if necessary.
-                      borderRadius: BorderRadius.circular(12),
-                      child: AspectRatio(
-                        aspectRatio: 500 / 750,
-                        child: itemList[index].posterPath != null
-                            ? CachedNetworkImage(
-                                imageUrl: baseUrl +
-                                    posterSize +
-                                    itemList[index].posterPath,
-                                progressIndicatorBuilder:
-                                    (context, url, progress) => Container(
-                                  color:
-                                      Theme.of(context).scaffoldBackgroundColor,
-                                  child: Center(
-                                    child: CircularProgressIndicator(
-                                      value: progress.progress,
+                        child: ClipRRect(
+                          //Removed Aspect Ratio. Add if necessary.
+                          borderRadius: BorderRadius.circular(12),
+                          child: AspectRatio(
+                            aspectRatio: 500 / 750,
+                            child: itemList[index].posterPath != null
+                                ? CachedNetworkImage(
+                                    imageUrl: baseUrl +
+                                        posterSize +
+                                        itemList[index].posterPath,
+                                    progressIndicatorBuilder:
+                                        (context, url, progress) => Container(
+                                      color: Theme.of(context)
+                                          .scaffoldBackgroundColor,
+                                      child: Center(
+                                        child: CircularProgressIndicator(
+                                          value: progress.progress,
+                                        ),
+                                      ),
+                                    ),
+                                    errorWidget: (context, url, error) =>
+                                        Icon(Icons.error),
+                                    fit: BoxFit.cover,
+                                  )
+                                : Container(
+                                    color: Theme.of(context)
+                                        .scaffoldBackgroundColor,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      // crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: <Widget>[
+                                        SizedBox.shrink(),
+                                        Material(
+                                          child: Text(
+                                            'Image not available',
+                                            style: TextStyle(
+                                              color: Colors.grey,
+                                            ),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                        Material(
+                                          child: Text(
+                                            itemList[index].name,
+                                            style: TextStyle(
+                                              color: Colors.grey,
+                                              fontSize: 12,
+                                            ),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                        SizedBox.shrink(),
+                                      ],
                                     ),
                                   ),
-                                ),
-                                errorWidget: (context, url, error) =>
-                                    Icon(Icons.error),
-                                fit: BoxFit.cover,
-                              )
-                            : Container(
-                                color:
-                                    Theme.of(context).scaffoldBackgroundColor,
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  // crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: <Widget>[
-                                    SizedBox.shrink(),
-                                    Material(
-                                      child: Text(
-                                        'Image not available',
-                                        style: TextStyle(
-                                          color: Colors.grey,
-                                        ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                    Material(
-                                      child: Text(
-                                        itemList[index].name,
-                                        style: TextStyle(
-                                          color: Colors.grey,
-                                          fontSize: 12,
-                                        ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                    SizedBox.shrink(),
-                                  ],
-                                ),
-                              ),
+                          ),
+                        ),
                       ),
-                    ),
+                      Text(
+                        itemList[index].name,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: GoogleFonts.lato(
+                          textStyle: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
               );
